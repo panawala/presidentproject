3ec9b8982aceb46958c6a13bbc28e433987c3ada
denghaotian219@gmail.com <denghaotian219@gmail.com@179067d1-1d96-9e15-5050-7be85a182e91>
2012-11-21 08:52:03 +0000

    dht 用frame作导航

---
+++ trunk/Decision Maker (University)/WpfApplication3/PageMaxTraffic.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WpfApplication3
+{
+    /// <summary>
+    /// Interaction logic for PageMaxTraffic.xaml
+    /// </summary>
+    public partial class PageMaxTraffic : Page
+    {
+        string strPATH;
+        public PageMaxTraffic()
+        {
+            InitializeComponent();
+            DirectoryInfo di;
+            di = new DirectoryInfo(System.Environment.CurrentDirectory);
+            strPATH = di.Parent.Parent.FullName;
+            webbrowserMaxTraffic.Navigate(new Uri(strPATH + @"/html/Traffic.htm", UriKind.RelativeOrAbsolute));
+        }
+
+        private void btn_minTraffic_Click(object sender, RoutedEventArgs e)
+        {
+            HomePage m_HomePage = new HomePage();
+            this.NavigationService.Navigate(m_HomePage);
+        }
+    }
+}
