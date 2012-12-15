48811d173149824469333d5b4ec3e82efcef63ee
denghaotian219@gmail.com <denghaotian219@gmail.com@179067d1-1d96-9e15-5050-7be85a182e91>
2012-12-15 13:17:37 +0000

    DHT，更新组织架构

---
+++ Decision Maker (University)/WpfApplication3/ResourcesAndManpower/PageFacultyStructure.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3.ResourcesAndManpower
+{
+    /// <summary>
+    /// Interaction logic for PageFacultyStructure.xaml
+    /// </summary>
+    public partial class PageFacultyStructure : Page
+    {
+        string strCollegeName;
+        public PageFacultyStructure(string College)
+        {
+            InitializeComponent();
+            strCollegeName = College;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblCollegeName.Content = strCollegeName;
+            DataSetFacultyStructureTableAdapters.T_FacultyStructureTableAdapter adapter = new DataSetFacultyStructureTableAdapters.T_FacultyStructureTableAdapter();
+            DataSetFacultyStructure.T_FacultyStructureDataTable dt = adapter.GetDataByCollegeAndMenu(strCollegeName,"现任领导");
+            listbox0.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "教学机构");
+            listbox1.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "研究机构");
+            listbox2.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "配套机构");
+            listbox3.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "管理机构");
+            listbox4.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "学术刊物");
+            listbox5.ItemsSource = dt;
+            dt = adapter.GetDataByCollegeAndMenu(strCollegeName, "国际机构");
+            listbox6.ItemsSource = dt;
+            
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            object obj = listbox0.ContainerFromElement((Hyperlink)sender);
+            int iCurrentItem = listbox0.Items.IndexOf(((ListBoxItem)obj).Content);
+            DataSetFacultyStructureTableAdapters.T_FacultyStructureTableAdapter adapter = new DataSetFacultyStructureTableAdapters.T_FacultyStructureTableAdapter();
+            DataSetFacultyStructure.T_FacultyStructureDataTable dt = adapter.GetDataByCollegeAndMenu(strCollegeName,"现任领导");
+            int PK_FacultyMember = dt[iCurrentItem].Id;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            string str = ((Hyperlink)sender).NavigateUri.ToString();
+        }
+    }
+}
---
+++ Decision Maker (University)/WpfApplication3/ResourcesAndManpower/PageOrganizationStructure.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication3.ResourcesAndManpower
+{
+    /// <summary>
+    /// Interaction logic for PageOrganizationStructure.xaml
+    /// </summary>
+    public partial class PageOrganizationStructure : Page
+    {
+        public PageOrganizationStructure()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            PageFacultyStructureMenu m_PageFacultyStructureMenu = new PageFacultyStructureMenu();
+            frame1.Navigate(m_PageFacultyStructureMenu);
+        }
+
+        private void tabControlOrganizationStructure_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.Source is TabControl)
+            {
+                switch (tabControlOrganizationStructure.SelectedIndex)
+                {
+                    case 0:
+                        if (frame1 != null)
+                        {
+                            PageFacultyStructureMenu m_page = new PageFacultyStructureMenu();
+                            frame1.Navigate(m_page);
+                        }
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+}
