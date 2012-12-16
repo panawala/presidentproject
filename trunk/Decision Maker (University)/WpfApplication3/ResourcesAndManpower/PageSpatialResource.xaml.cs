bad6aa5a42aeb9be20bbce97c347f8b430098a84
denghaotian219@gmail.com <denghaotian219@gmail.com@179067d1-1d96-9e15-5050-7be85a182e91>
2012-12-16 07:01:04 +0000

    dht 组织架构

--- trunk/Decision Maker (University)/WpfApplication3/ResourcesAndManpower/PageSpatialResource.xaml.cs
+++ trunk/Decision Maker (University)/WpfApplication3/ResourcesAndManpower/PageSpatialResource.xaml.cs
@@ -30,37 +30,45 @@ namespace WpfApplication3.ResourcesAndManpower
                 BuildingIntro="BuildingIntro",
                 CurrentUse="CurrentUse",
                 Organization="vOrganization",
-                RecentActivity="RecentActivity"
+                RecentActivity="RecentActivity",
+                Top=221,
+                Left=540
             },
              new SpatialNode
             {
                 Name="西北四楼",
-                Type="教学办公用楼",
+                Type="食宿用楼",
                 ImageUrl="Image/sdf",
                 BuildingIntro="BuildingIntro",
                 CurrentUse="CurrentUse",
                 Organization="vOrganization",
-                RecentActivity="RecentActivity"
+                RecentActivity="RecentActivity",
+                Top=111,
+                Left=243
             },
              new SpatialNode
             {
                 Name="图书馆",
-                Type="设施与实验室",
+                Type="设施和实验室",
                 ImageUrl="Image/sdf",
                 BuildingIntro="BuildingIntro",
                 CurrentUse="CurrentUse",
                 Organization="vOrganization",
-                RecentActivity="RecentActivity"
+                RecentActivity="RecentActivity",
+                Top=308,
+                Left=509
             },
              new SpatialNode
             {
                 Name="游泳馆",
-                Type="体育用楼",
+                Type="体育场馆",
                 ImageUrl="Image/sdf",
                 BuildingIntro="BuildingIntro",
                 CurrentUse="CurrentUse",
                 Organization="vOrganization",
-                RecentActivity="RecentActivity"
+                RecentActivity="RecentActivity",
+                Top=498,
+                Left=343
             }
         };
 
@@ -101,6 +109,86 @@ namespace WpfApplication3.ResourcesAndManpower
                 return null;
         }
 
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            string strCollegeName = btn.Content.ToString();
+            SpatialNode node = getSpatialNodeByName(strCollegeName);
+            if (System.IO.File.Exists(node.ImageUrl))
+            {
+                BitmapImage image = new BitmapImage(new Uri(node.ImageUrl));
+                imageBuilding.Source = image;
+            }
+        }
+
+        private void btnAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in canvasMap.Children)
+            {
+                btn.IsEnabled = true;
+            }
+        }
+
+        private void btnTeachingOffice_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in canvasMap.Children)
+            {
+                if (getSpatialNodeByName(btn.Content.ToString()).Type == "教学办公用楼")
+                    btn.IsEnabled = true;
+                else
+                    btn.IsEnabled = false;
+            }
+        }
+
+        private void btnAccommodation_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in canvasMap.Children)
+            {
+                if (getSpatialNodeByName(btn.Content.ToString()).Type == "食宿用楼")
+                    btn.IsEnabled = true;
+                else
+                    btn.IsEnabled = false;
+            }
+        }
+
+        private void btnFacilitiesAndLaboratories_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in canvasMap.Children)
+            {
+                if (getSpatialNodeByName(btn.Content.ToString()).Type == "设施和实验室")
+                    btn.IsEnabled = true;
+                else
+                    btn.IsEnabled = false;
+            }
+        }
+
+        private void btnStadium_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button btn in canvasMap.Children)
+            {
+                if (getSpatialNodeByName(btn.Content.ToString()).Type == "体育场馆")
+                    btn.IsEnabled = true;
+                else
+                    btn.IsEnabled = false;
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (SpatialNode node in spatialNodes)
+            {
+                Button btn = new Button();
+                btn.Width = 75;
+                btn.Height = 23;
+                btn.Content = node.Name;
+                btn.Click += button_Click;
+                canvasMap.Children.Add(btn);
+                canvasMap.RegisterName("btn" + node.Name, btn);
+                Canvas.SetLeft(btn, node.Left);
+                Canvas.SetTop(btn, node.Top);
+            }
+        }
+
 
 
 
@@ -123,6 +211,8 @@ namespace WpfApplication3.ResourcesAndManpower
         public string CurrentUse { get; set; }
         public string Organization { get; set; }
         public string RecentActivity { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
     }
 
 }
