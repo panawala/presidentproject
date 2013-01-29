3fc58ca4431fc33816751b97d73245b135293d50
formarkandroid <formarkandroid@179067d1-1d96-9e15-5050-7be85a182e91>
2013-01-29 03:16:47 +0000

    师资水平改进，增加链接 弹框

---
+++ trunk/Decision Maker (University)/WpfApplication3/Development performance/Teaching.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication3.Development_performance
+{
+    /// <summary>
+    /// Teaching.xaml 的交互逻辑
+    /// </summary>
+    public partial class Teaching : Window
+    {
+        public Teaching(String str)
+        {
+            InitializeComponent();
+            try
+            {
+                webBrowser1.Navigate(new Uri(str, UriKind.Absolute));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
